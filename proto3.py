628bc4509695de4ca59d7d28eb0aef431978c94a
christian.loelkes <christian@loelkes.com>
2016-01-05 04:07:34 +0100

    Test

--- proto3.py
+++ proto3.py
@@ -4,72 +4,78 @@ __author__ = 'loelkes'
 
 import json, sys, os, time
 import subprocess
-import config
+from woocommerce import API
+
+#import config
+
+import ConfigParser
+settings = ConfigParser.ConfigParser()
+
+settings.read('config.ini')
+
+WebAPI = API(
+        settings.get('WC', 'URL'),
+        settings.get('WC', 'Key'),
+        settings.get('WC', 'Secret'),
+        version = settings.get('WC', 'Version')
+)
+
+# Configuration for API Access
+WebAPI.APILimit = settings.get('WC', 'APILimit')
+WebAPI.APIFields = settings.get('WC', 'APIFields')
 
 # Configure the script here
 
 # openscad = '/Applications/OpenSCAD.app/Contents/MacOS/./OpenSCAD'   # If used on OSX
-openscad = 'C:\\Program Files\\OpenSCAD\\openscad.com'  # If used on Windows
+# openscad = 'C:\\Program Files\\OpenSCAD\\openscad.com'  # If used on Windows
 
-extension = '.stl'  # Extensions used for output of OpenSCAD
-output_folder = 'D:\\BottleClips-Script\\Tags\\'  # Relative to the script location
+# extension = '.stl'  # Extensions used for output of OpenSCAD
+# output_folder = 'D:\\BottleClips-Script\\Tags\\'  # Relative to the script location
 
-openscad_file_path = 'C:\\Users\\kinect\\ownCloud\\Projekte\\3DPrinter\\OpenSCAD\\Mate_Tag'
+# openscad_file_path = 'C:\\Users\\kinect\\ownCloud\\Projekte\\3DPrinter\\OpenSCAD\\Mate_Tag'
 # openscad_file = '../31C3-bottle-clip.scad'                        # OpenSCAD File used
-openscad_file = openscad_file_path + '\\' + 'CCCamp15-bottle-tag.scad'
-
-# All the colors in the DB
-# ToDo: get the colors from the DB
-colorDB = ['Glow in the dark', 'Random (always fallback option)']
-
-# Configuration for API Access
-config.APILimit = "200"
-config.APIFields = "id,line_items,status"
+# openscad_file = openscad_file_path + '\\' + 'CCCamp15-bottle-tag.scad'
 
 start_from_order = 0
+product_id = 278
+colors = []
 
-def getAPIData():
-    api_r = config.wcapi.get("orders?filter[limit]=" + config.APILimit + "&fields=" + config.APIFields).text
-    return json.loads(api_r)
+def getAPIOrders():
+    orders_raw = WebAPI.get("orders?filter[limit]=" + WebAPI.APILimit + "&fields=" + WebAPI.APIFields).text
+    return json.loads(orders_raw)
 
+def getAPIProducts():
+    products_raw = WebAPI.get("products?fields=variations,title").text
+    return json.loads(products_raw)
 
 def now():
     return time.strftime('%X %x %Z')
 
-
 # Filter all the orders
 def filterOrders(orders):
     mate_tags = []
+    global colors
     global start_from_order
+    global product_id
 
     # Filter out all mate tag orders
     for order in orders['orders']:
-        if order['status'] == 'processing' and order['id'] >= int(start_from_order):  # Get only the open orders
-            for item in order['line_items']:  # Get each item in the order
-                if item['product_id'] == 278:  # Only get the mate tags
+        if order['status'] == 'processing' and order['id'] >= int(start_from_order):    # Get only the open orders
+            for item in order['line_items']:                                            # Get each item in the order
+                if item['product_id'] == int(product_id):                           # Only get the mate tags
                     new_item = [item['meta'][0]['value'],   # Color
                         item['meta'][1]['value'],           # Bottle Type
                         item['meta'][2]['value'],           # Nick
                         item['meta'][3]['value'],           # Icons
                         order['id']                         # Order ID
-                        ]
-                    if new_item[3].find('#') >= 1:
+                    ]
+                    if new_item[3].find('#') != -1:         # Set all icons to random if # is found.
                         new_item[3] = '#'
                     mate_tags.append(new_item)
-                    print new_item
+                    print new_item[0] + ' ' + new_item[1] + ' ' + new_item[2]
+                    if new_item[0] not in colors:
+                        colors.append(new_item[0])
     return mate_tags
-
-
-# def getOrderNick(orders):
-#     data = dict()
-#     for order in orders['orders']:
-#         order_id = order['id']
-#         if order['status'] == 'processing':
-#             for item in order['line_items']:
-#                 if item['product_id'] == 278:
-#                     values = {'id': order_id, 'nickname': item['meta'][3]['value']}
-#                     print(values)
-
 
 # Filter array by colors and output array containing only the given color
 def filterColors(data, color):
@@ -96,50 +102,62 @@ def tags2openscad(data):
         filename = active_color.replace(" ", "") + '_' + tagset[0][2].replace(" ", "") + '_' + tagset[1][2].replace(" ","")
         print(' Generating Tags: ' + filename + extension)
         # print('Current order ID: ' + str(tagset[0][4]))
-        subprocess.call(openscad + ' -o ' + output_folder + filename + extension
-                        + ' -D "name1="""' + tagset[0][2] + '"" '
-                        + ' -D "icons1="""' + tagset[0][3] + '"" '
-                        + ' -D "type1="""' + tagset[0][1].title() + '"" '
-                        + ' -D "name2="""' + tagset[1][2] + '"" '
-                        + ' -D "icons2="""' + tagset[1][3] + '"" '
-                        + ' -D "type2="""' + tagset[1][1].title() + '"" '
-                        + openscad_file)
+        if settings.get('General', 'System') == 'Windows':
+            subprocess.call(openscad + ' -o ' + output_folder + filename + extension
+                + ' -D "name1="""' + tagset[0][2] + '"" '
+                + ' -D "icons1="""' + tagset[0][3] + '"" '
+                + ' -D "type1="""' + tagset[0][1].title() + '"" '
+                + ' -D "name2="""' + tagset[1][2] + '"" '
+                + ' -D "icons2="""' + tagset[1][3] + '"" '
+                + ' -D "type2="""' + tagset[1][1].title() + '"" '
+                + openscad_file)
+        elif settings.get('General', 'System') == 'OSX':
+            os.system(openscad + ' -o ' + output_folder + filename + extension
+                + ' -D \'name1="' + tagset[0][2] + '"\''
+                + ' -D \'icons1="' + tagset[0][3] +'"\''
+                + ' -D \'type1="' + tagset[0][1].title() + '"\''
+                + ' -D \'name2="' + tagset[1][2] + '"\''
+                + ' -D \'icons2="' + tagset[1][3] +'"\''
+                + ' -D \'type2="' + tagset[1][1].title() + '"\' '+
+                openscad_file + ' > /dev/null 2>&1')
 
-def stats(tags):
-    print()
-    print("------ Begin Data Stats ------")
-    for color in colorDB:
-        count = len(filterColors(tags, color))
-        print(color + ': ' + str(count))
+
+
+def stats(tags, colors):
+    print("\n------ Begin Data Stats ------\n")
     print('Total unique tags in the queue: ' + str(len(tags)))
-    print("------ End Data Stats ------")
-    print("")
+    print('Total colors: ' + str(len(colors)) + '\n')
+    index = 0
+    for color in colors:
+        count = len(filterColors(tags, color))
+        print(str(index) + ' ' + color + ': ' + str(count))
+        index += 1
+    print("\n------ End Data Stats ------\n")
 
+def main():
+    print("\n------ Proto3.de Bottle Tag script ------\n")
+    start_from_order = raw_input("Please enter starting order ID: ")
+    product_id = raw_input("Please enter product ID: ")
+    print("\n------ Fetching data from API, please wait ------\n")
+    global APIdata
+    APIdata = getAPIOrders()
+    global tags
+    tags = filterOrders(APIdata)
+    stats(tags, colors)
 
-print(" ")
-print("------ Proto3.de Bottle Tag script ------")
-print(" ")
-start_from_order = raw_input("Please enter starting order ID: ")
+    color_input = raw_input("Please enter color ID to generate tags: ")
+    if color_input < len(colors):
+        active_color = color_input
+    else:
+        print("Error: Color \"" + color_input + "\" not found in database.")
+        sys.exit(1)
 
-global APIdata
-APIdata = getAPIData()
-global tags
-tags = filterOrders(APIdata)
-stats(tags)
+    print("------ Start ------")
+    print('starting with order: ' + start_from_order)
+    print("Generating tags with the color: " + active_color)
 
-color_input = raw_input("Please enter color to generate tags: ")
-if color_input in colorDB:
-    active_color = color_input
-else:
-    print("Error: Color \"" + color_input + "\" not found in database.")
-    sys.exit(1)
+    colorset = filterColors(tags, active_color)
+    colorpairs = pair2tags(colorset)
+    tags2openscad(colorpairs)
 
-print("------ Start ------")
-print('starting with order: ' + start_from_order)
-print("Generating tags with the color: " + active_color)
-
-colorset = filterColors(tags, active_color)
-colorpairs = pair2tags(colorset)
-tags2openscad(colorpairs)
-
-# sys.exit(0)
+main()
